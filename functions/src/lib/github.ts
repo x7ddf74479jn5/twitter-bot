fe15aaf7b4a64b874e8bf2b283c7ee341500525d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-04-27 21:18:42 +0900

    🐛 fix: GitHub API

--- functions/src/lib/github.ts
+++ functions/src/lib/github.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import type { Dayjs } from "dayjs";
 import ja from "dayjs/locale/ja";
 import * as functions from "firebase-functions";
+import got from "got";
 
 import { GitHubAPIResponse, ContributionWeek, ContributionDay } from "../types";
 
@@ -11,14 +12,15 @@ const GITHUB_READ_USER_TOKEN = functions.config().github.read_user_token;
 const GITHUB_USER = "x7ddf74479jn5";
 
 const query = `
-query ($userName: String!,$date: DateTime!) {
+query ($userName: String!, $dateTime: DateTime!) {
   user(login: $userName) {
-    contributionsCollection(from: $date, to: $date) {
+    contributionsCollection(from: $dateTime, to: $dateTime) {
       contributionCalendar {
         totalContributions
         weeks {
           contributionDays {
             contributionCount
+            date
           }
         }
       }
@@ -29,8 +31,8 @@ query ($userName: String!,$date: DateTime!) {
 
 const getVariables = (date: Dayjs) => {
   return `{
-  "userName": ${GITHUB_USER},
-  "date": "${date.toISOString()}"
+  "userName": "${GITHUB_USER}",
+  "dateTime": "${date.format()}"
 }
 `;
 };
@@ -40,7 +42,6 @@ const yesterday = dayjs().subtract(1, "day");
 const json = { query, variables: getVariables(yesterday) };
 
 export const getContributionWeek = async (): Promise<ContributionWeek> => {
-  const got = await (await import("got")).default;
   const { data }: GitHubAPIResponse = await got
     .post(url, {
       headers: { Authorization: `Bearer ${GITHUB_READ_USER_TOKEN}` },
