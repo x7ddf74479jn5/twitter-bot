3e90f0734c4ac24465738b071fca8a6c07bb3c72
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-07 21:04:31 +0900

    ✏️ typo

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -7,8 +7,8 @@ const GITHUB_ENDPOINT = "https://api.github.com/graphql";
 export { GITHUB_READ_USER_TOKEN, GITHUB_USER, GITHUB_ENDPOINT };
 
 // Twitter
-const TWITTER_CONSUMER_KEY = functions.config().twitter.consumer_key;
-const TWITTER_CONSUMER_SECRET = functions.config().twitter.consumer_secret;
+const TWITTER_APP_KEY = functions.config().twitter.app_key;
+const TWITTER_APP_SECRET = functions.config().twitter.app_secret;
 const TWITTER_ACCESS_TOKEN = functions.config().twitter.access_token;
 const TWITTER_ACCESS_SECRET = functions.config().twitter.access_secret;
-export { TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET };
+export { TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET };
--- functions/src/lib/twitter.ts
+++ functions/src/lib/twitter.ts
@@ -1,14 +1,9 @@
 import TwitterApi from "twitter-api-v2";
-import {
-  TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET,
-  TWITTER_ACCESS_TOKEN,
-  TWITTER_ACCESS_SECRET,
-} from "../constants";
+import { TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET } from "../constants";
 
 export const twitterClient = new TwitterApi({
-  appKey: TWITTER_CONSUMER_KEY,
-  appSecret: TWITTER_CONSUMER_SECRET,
+  appKey: TWITTER_APP_KEY,
+  appSecret: TWITTER_APP_SECRET,
   accessToken: TWITTER_ACCESS_TOKEN,
   accessSecret: TWITTER_ACCESS_SECRET,
 });
