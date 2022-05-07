44b41bd4173eb48f06466feb870a88ee4c6394f5
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-05-07 23:02:22 +0900

    🔧 config: firebase

---
+++ config/firebase.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+};
